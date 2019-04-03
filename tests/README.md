edde63b03ff3cd4555e8da3459d04044d5b2338c
kakuilan <kakuilan@163.com>
2019-04-03 16:00:20 +0800

    init project

---
+++ tests/README.md
@@ -0,0 +1,6 @@
+# test
+e.g.
+
+phpunit --bootstrap bootstrap.php --repeat 100  XxxTest.php
+
+phpunit --repeat 100  XxxTest.php
---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright (c) 2019 LKK/lianq.net All rights reserved
+ * User: kakuilan@163.com
+ * Date: 2019/4/3
+ * Time: 15:52
+ * Desc:
+ */
+
+define('DS', str_replace('\\', '/', DIRECTORY_SEPARATOR));
+define('PS', PATH_SEPARATOR);
+define('TESTDIR', str_replace('\\', '/', __DIR__ . DS ) ); //根目录
+
+$loader = require __DIR__ .'/../vendor/autoload.php';
