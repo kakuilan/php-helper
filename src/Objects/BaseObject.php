0495baf022718b2248a904d5b27295eec070e141
kakuilan <kakuilan@163.com>
2019-12-24 10:46:26 +0800

    add BaseObject:getSelfInstance/getFinalInstance等

--- src/Objects/BaseObject.php
+++ src/Objects/BaseObject.php
@@ -11,6 +11,24 @@ namespace Kph\Objects;
 
 class BaseObject {
 
+    /**
+     * 最终子类实例化
+     * @var object
+     */
+    protected static $_self;
+
+
+    /**
+     * 最终父类实例化
+     * @var object
+     */
+    protected static $_final;
+
+
+    /**
+     * 打印时转字符串
+     * @return string
+     */
     public function __toString(){
         return get_class($this);
     }
@@ -24,6 +42,76 @@ class BaseObject {
         $arr = explode('\\', get_class($this));
         return end($arr);
     }
+
+
+    /**
+     * 实例化并返回[调用此方法的类,静态绑定]
+     * 不可重写
+     * @return object
+     */
+    final public static function getSelfInstance() {
+        if(is_null(static::$_self) || !is_object(static::$_self) || !(static::$_self instanceof static)) {
+            //静态延迟绑定
+            static::$_self = new static();
+        }
+
+        return static::$_self;
+    }
+
+
+    /**
+     * 是否存在当前[调用]类实例化
+     * 不可重写
+     * @return bool
+     */
+    final public static function hasSelfInstance() {
+        return isset(static::$_self);
+    }
+
+
+    /**
+     * 销毁当前[调用]类实例化
+     * 不可重写
+     */
+    final public static function destroySelfInstance() {
+        static::$_self = null;
+    }
+
+
+    /**
+     * 实例化并返回[最终父类]
+     * 可重写,返回所重写的类
+     * 当前返回BaseObject
+     * @return BaseObject
+     */
+    public static function getFinalInstance() {
+        if(is_null(self::$_final) || !is_object(self::$_final) || !(self::$_final instanceof self)) {
+            self::$_final = new self();
+        }
+
+        return self::$_final;
+    }
+
+
+    /**
+     * 是否存在最终类实例化
+     * 可重写
+     * @return bool
+     */
+    public static function hasFinalInstance() {
+        return isset(self::$_final);
+    }
+
+
+    /**
+     * 销毁最终类实例化
+     * 可重写
+     */
+    public static function destroyFinalInstance() {
+        self::$_final = null;
+    }
+
+
 
 
 }
--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -9,9 +9,9 @@
 
 namespace Kph\Services;
 
-use Kph\Objects\BaseObject;
+use Kph\Objects\StrictObject;
 
-class BaseService extends BaseObject {
+class BaseService extends StrictObject {
 
 
     /**
@@ -27,19 +27,6 @@ class BaseService extends BaseObject {
      */
     public $error;
 
-
-    /**
-     * 供静态绑定
-     * @var object
-     */
-    protected static $instance;
-
-
-    /**
-     * 供最终子类绑定
-     * @var object
-     */
-    protected static $_instance;
 
 
     /**
@@ -61,49 +48,10 @@ class BaseService extends BaseObject {
 
 
     /**
-     * 实例化并返回[父类]
-     * @param array $vars
-     * @return mixed
-     */
-    public static function instance(array $vars = []) {
-        if(is_null(self::$instance) || !is_object(self::$instance) || !(self::$instance instanceof self)) {
-            self::$instance = new self($vars);
-        }
-
-        return self::$instance;
-    }
-
-
-    /**
-     * 实例化并返回[静态绑定,供(当前)子类调用]
-     * @param array $vars
-     *
-     * @return mixed
-     */
-    public static function getInstance(array $vars = []) {
-        if(is_null(static::$_instance) || !is_object(static::$_instance) || !(static::$_instance instanceof static)) {
-            //静态延迟绑定
-            static::$_instance = new static($vars);
-        }
-
-        return static::$_instance;
-    }
-
-
-    /**
-     * 销毁实例化对象
-     */
-    public static function destroy() {
-        self::$instance = null;
-        static::$_instance = null;
-    }
-
-
-    /**
      * 获取错误代码
      * @return mixed
      */
-    public function errno() {
+    public function getErrno() {
         return $this->errno;
     }
 
@@ -112,27 +60,27 @@ class BaseService extends BaseObject {
      * 获取错误信息
      * @return mixed
      */
-    public function error() {
+    public function getError() {
         return $this->error;
     }
 
 
     /**
-     * 设置服务错误
+     * 设置服务错误信息
      * @param string $error 错误信息
      * @param string|number $errno 错误代码
      */
-    public function setError(string $error='', $errno=null) {
+    public function setErrorInfo(string $error='', $errno=null) {
         $this->error = $error;
         $this->errno = $errno;
     }
 
 
     /**
-     * 获取服务错误
+     * 获取服务错误信息
      * @return array
      */
-    public function getError() {
+    public function getErrorInfo() {
         return [
             'errno' => $this->errno,
             'error' => $this->error,
