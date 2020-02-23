4d8345c6a5839e435f670444740dd317f4a5ca79
kakuilan <kakuilan@163.com>
2020-02-23 13:37:52 +0800

    add testMkdirDeep/testGetFileTree

--- tests/Unit/DirectoryHelperTest.php
+++ tests/Unit/DirectoryHelperTest.php
@@ -18,4 +18,20 @@ use Kph\Helpers\DirectoryHelper;
 class DirectoryHelperTest extends TestCase {
 
 
+    public function testMkdirDeep() {
+        $dir = TESTDIR . 'tmp/test/2020/0223';
+        $res = DirectoryHelper::mkdirDeep($dir);
+        $this->assertTrue($res);
+    }
+
+
+    public function testGetFileTree() {
+        $all   = DirectoryHelper::getFileTree(TESTDIR);
+        $dirs  = DirectoryHelper::getFileTree(TESTDIR, 'dir');
+        $files = DirectoryHelper::getFileTree(TESTDIR, 'file');
+        $this->assertEquals(count($all), count($dirs) + count($files));
+
+        DirectoryHelper::getFileTree(TESTDIR, 'file', true);
+    }
+
 }
