543fbd6fabae1e5877bbb2858c4e357898b3539f
kakuilan <kakuilan@163.com>
2020-02-24 16:10:49 +0800

    fix 完成OsHelper

--- src/Helpers/OsHelper.php
+++ src/Helpers/OsHelper.php
@@ -8,6 +8,7 @@
  */
 
 namespace Kph\Helpers;
+
 use Kph\Consts;
 
 /**
@@ -218,6 +219,151 @@ class OsHelper {
         }
 
         return $os;
+    }
+
+
+    /**
+     * 获取客户端IP
+     * @param array $server server信息
+     * @return string
+     */
+    public static function getClientIp(array $server = []): string {
+        if (empty($server)) {
+            $server = $_SERVER;
+        }
+
+        if (!empty($server)) {
+            //获取代理ip
+            if (isset($server["HTTP_X_FORWARDED_FOR"]) && preg_match_all('#(\d+\.){3}\d+#', $server['HTTP_X_FORWARDED_FOR'], $matches)) {
+                foreach ($matches[0] AS $xip) {
+                    if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
+                        $ip = $xip;
+                        break;
+                    }
+                }
+            } else if (isset($server["HTTP_CLIENT_IP"])) {
+                $ip = $server["HTTP_CLIENT_IP"];
+            } else {
+                $ip = $server["REMOTE_ADDR"];
+            }
+        } else {
+            if (getenv("HTTP_X_FORWARDED_FOR")) {
+                $ip = getenv("HTTP_X_FORWARDED_FOR");
+            } else if (getenv("HTTP_CLIENT_IP")) {
+                $ip = getenv("HTTP_CLIENT_IP");
+            } else {
+                $ip = getenv("REMOTE_ADDR");
+            }
+        }
+
+        preg_match("/[\d\.]{7,15}/", $ip, $matches);
+        $ip = $matches[0] ?? '0.0.0.0';
+
+        return $ip;
+    }
+
+
+    /**
+     * 获取服务器IP
+     * @param array $server server信息
+     * @return string
+     */
+    public static function getServerIP(array $server = []): string {
+        if (empty($server)) {
+            $server = $_SERVER;
+        }
+
+        if (!empty($server)) {
+            if (isset($server['SERVER_ADDR'])) {
+                $serverIp = $server['SERVER_ADDR'];
+            } else {
+                $serverIp = $server['LOCAL_ADDR'];
+            }
+        } elseif (getenv('SERVER_ADDR')) {
+            $serverIp = getenv('SERVER_ADDR');
+        } else {
+            $serverIp = gethostbyname(gethostname());
+        }
+
+        if (!filter_var($serverIp, FILTER_VALIDATE_IP)) {
+            $serverIp = '0.0.0.0';
+        }
+
+        return $serverIp;
+    }
+
+
+    /**
+     * 获取域名
+     * @param string $url
+     * @param bool $firstLevel 是否获取一级域名,如:abc.test.com取test.com
+     * @param array $server server信息
+     * @return string
+     */
+    public static function getDomain(string $url, bool $firstLevel = false, array $server = []): string {
+        if (empty($server)) {
+            $server = $_SERVER;
+        }
+        if (empty($url)) {
+            $url = $server['HTTP_HOST'] ?? '';
+        }
+
+        if (!stripos($url, '://')) {
+            $url = 'http://' . $url;
+        }
+
+        $parse  = parse_url(strtolower($url));
+        $domain = null;
+        if (isset($parse['host'])) {
+            $domain = $parse['host'];
+        }
+
+        if ($firstLevel) {
+            $arr  = explode('.', $domain);
+            $size = count($arr);
+            if ($size >= 2) {
+                $domain = $arr[$size - 2] . '.' . end($arr);
+            }
+        }
+
+        return $domain;
+    }
+
+
+    /**
+     * 获取当前页面完整URL地址
+     * @param array $server server信息
+     * @return string
+     */
+    public static function getUrl(array $server = []) {
+        if (empty($server)) {
+            $server = $_SERVER;
+        }
+
+        $protocal  = ($server['SERVER_PORT'] ?? '') == '443' ? 'https://' : 'http://';
+        $phpSelf   = $server['PHP_SELF'] ?? $server['SCRIPT_NAME'];
+        $pathInfo  = $server['PATH_INFO'] ?? '';
+        $relateUrl = $server['REQUEST_URI'] ?? $phpSelf . (isset($server['QUERY_STRING']) ? '?' . $server['QUERY_STRING'] : $pathInfo);
+        return $protocal . ($server['HTTP_HOST'] ?? '') . $relateUrl;
+    }
+
+
+    /**
+     * 获取URI
+     * @param array $server
+     * @return string
+     */
+    public static function getUri(array $server = []): string {
+        if (empty($server)) {
+            $server = $_SERVER;
+        }
+
+        if (isset($server['REQUEST_URI'])) {
+            return $server['REQUEST_URI'];
+        }
+
+        $uri = ($server['PHP_SELF'] ?? '') . "?" . ($server['argv'][0] ?? ($server['QUERY_STRING'] ?? ''));
+        return $uri;
     }
 
 
