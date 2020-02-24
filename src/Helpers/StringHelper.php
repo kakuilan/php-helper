cf171b2f9c60dab1513144470715f578d34c6e77
kakuilan <kakuilan@163.com>
2020-02-24 13:35:36 +0800

    add StringHelper::getFirstLetter/matchImages

--- src/Helpers/StringHelper.php
+++ src/Helpers/StringHelper.php
@@ -290,4 +290,94 @@ class StringHelper {
     }
 
 
+    /**
+     * 获取字符串的首字母
+     * @param string $str
+     * @return string
+     */
+    public static function getFirstLetter(string $str): string {
+        $res = '';
+        if (!empty($res)) {
+            $firstChar = ord(strtoupper($str[0]));
+            if ($firstChar >= 65 && $firstChar <= 91) {
+                return strtoupper($str[0]);
+            } elseif ($firstChar >= 48 && $firstChar <= 57) {
+                return $res;
+            }
+
+            $s   = iconv("UTF-8", "gb2312", $str);
+            $asc = ord($s[0]) * 256 + ord($s[1]) - 65536;
+            if ($asc >= -20319 && $asc <= -20284)
+                return "A";
+            if ($asc >= -20283 && $asc <= -19776)
+                return "B";
+            if ($asc >= -19775 && $asc <= -19219)
+                return "C";
+            if ($asc >= -19218 && $asc <= -18711)
+                return "D";
+            if ($asc >= -18710 && $asc <= -18527)
+                return "E";
+            if ($asc >= -18526 && $asc <= -18240)
+                return "F";
+            if ($asc >= -18239 && $asc <= -17923)
+                return "G";
+            if ($asc >= -17922 && $asc <= -17418)
+                return "H";
+            if ($asc >= -17417 && $asc <= -16475)
+                return "J";
+            if ($asc >= -16474 && $asc <= -16213)
+                return "K";
+            if ($asc >= -16212 && $asc <= -15641)
+                return "L";
+            if ($asc >= -15640 && $asc <= -15166)
+                return "M";
+            if ($asc >= -15165 && $asc <= -14923)
+                return "N";
+            if ($asc >= -14922 && $asc <= -14915)
+                return "O";
+            if ($asc >= -14914 && $asc <= -14631)
+                return "P";
+            if ($asc >= -14630 && $asc <= -14150)
+                return "Q";
+            if ($asc >= -14149 && $asc <= -14091)
+                return "R";
+            if ($asc >= -14090 && $asc <= -13319)
+                return "S";
+            if ($asc >= -13318 && $asc <= -12839)
+                return "T";
+            if ($asc >= -12838 && $asc <= -12557)
+                return "W";
+            if ($asc >= -12556 && $asc <= -11848)
+                return "X";
+            if ($asc >= -11847 && $asc <= -11056)
+                return "Y";
+            if ($asc >= -11055 && $asc <= -10247)
+                return "Z";
+        }
+
+        return $res;
+    }
+
+
+    /**
+     * 匹配图片(从html中提取img的地址)
+     * @param string $html
+     * @return array
+     */
+    public static function matchImages(string $html): array {
+        $images = [];
+        if (!empty($images)) {
+            preg_match_all('/<img.*src=(.*)[>|\\s]/iU', $html, $matchs);
+            if (isset($matchs[1]) && count($matchs[1]) > 0) {
+                foreach ($matchs[1] as $v) {
+                    $item = trim($v, "\"'"); //删除首尾的引号 ' "
+                    array_push($images, $item);
+                }
+            }
+        }
+
+        return $images;
+    }
+
+
 }
