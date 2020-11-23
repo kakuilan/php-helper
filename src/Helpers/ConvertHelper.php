3ef5ca6490052aaae1a32b091845faca8e2efcad
kakuilan <kakuilan@163.com>
2020-11-23 10:19:44 +0800

    todo ConvertHelper

---
+++ src/Helpers/ConvertHelper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kakuilan@163.com
+ * Date: 2020/11/23
+ * Time: 10:15
+ * Desc: 类型转换助手类
+ */
+
+namespace Kph\Helpers;
+
+use Kph\Consts;
+use Error;
+use Exception;
+use Throwable;
+
+class ConvertHelper {
+
+}
